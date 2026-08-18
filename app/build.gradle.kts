f59056063515b79c49501905ef841c4ded143753
mikaellferreiraalves-cloud <mikaellferreiraalves@gmail.com>
2026-08-18 18:49:27 -0300

    Remove Kotlin Android plugin from build script
    Removed Kotlin Android plugin from build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 import com.google.gms.googleservices.GoogleServicesPlugin.MissingGoogleServicesStrategy
 
 plugins {
-  alias(libs.plugins.kotlin.android)
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.compose)
   alias(libs.plugins.google.devtools.ksp)
